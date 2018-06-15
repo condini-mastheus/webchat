056a8326830c1aa47ed46c93c3be87afb97a72fe
condini-mastheus <matheus.condini@gmail.com>
2018-06-15 17:17:51 -0300

    Start with webscoket and socket-io

--- index.js
+++ index.js
@@ -7,11 +7,30 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+io.on('connection', function (socket) {
+  socket.broadcast.emit('hi');
+
+  socket.on('chat message', function (data) {
+    io.emit('chat message', {
+      message: data.message,
+      user: data.user
+    });
+  });
+
+  socket.on('typing', function (user) {
+    socket.broadcast.emit('typing', user);
+  });
+
+  socket.on('notTyping', function (user) {
+    socket.broadcast.emit('notTyping', user);
+  });
+
+  socket.on('disconnect', function () {
+    socket.broadcast.emit('bye');
   });
 });
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
